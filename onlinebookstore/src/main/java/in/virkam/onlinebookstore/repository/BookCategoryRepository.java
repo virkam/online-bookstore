109d9f1bbb7d63848742349dbd9a309e49d786eb
91909 <91909@LAPTOP-L3KQ73J6>
2021-05-10 23:42:36 +0530

    Java Code changes commits

---
+++ onlinebookstore/src/main/java/in/virkam/onlinebookstore/repository/BookCategoryRepository.java
@@ -0,0 +1,13 @@
+package in.virkam.onlinebookstore.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import in.virkam.onlinebookstore.entity.BookCategory;
+
+ 
+@RepositoryRestResource(collectionResourceRel="bookCateogry", path="book-category")
+public interface BookCategoryRepository extends JpaRepository<BookCategory, Long>{
+
+}
