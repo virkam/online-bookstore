3c108e62e2920561313d2b6a19da093998d82b89
91909 <91909@LAPTOP-L3KQ73J6>
2021-06-28 02:08:45 +0530

    Completed Feature 7 : Pagination server side : Customization done

--- angular-bookstore/src/app/components/book-list/book-list.component.ts
+++ angular-bookstore/src/app/components/book-list/book-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Book } from 'src/app/common/book';
 import { BookService } from 'src/app/services/book.service';
+import {NgbPaginationConfig} from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-book-list',
@@ -11,180 +13,213 @@ import { BookService } from 'src/app/services/book.service';
 })
 export class BookListComponent implements OnInit {
 
-  books: Book[] ;
-  currentCategoryId : number ;
-  searchMode : boolean;
-  constructor(private _bookService : BookService,
-         private _activatedRoute : ActivatedRoute
-    ) { }
+  books: Book[] = [];
+  currentCategoryId: number = 1;
+  searchMode: boolean = false;
+  previousCategory : number = 1;
+  // new properties for serverside paging 
+  currentPage: number = 1;
+  pageSize: number = 5;
+  totalRecords: number = 0;
+
+
+  constructor(private _bookService: BookService,
+    private _activatedRoute: ActivatedRoute,
+    _config : NgbPaginationConfig 
+  ) {
+        _config.maxSize = 3;
+        _config.boundaryLinks = true;
+   }
+
 
   ngOnInit(): void {
-    this._activatedRoute.paramMap.subscribe(()=>{
-          this.listBooks();
+    this._activatedRoute.paramMap.subscribe(() => {
+      this.listBooks();
     });
   }
 
-  listBooks(){
-      this.searchMode = this._activatedRoute.snapshot.paramMap.has('keyword');
-    if(this.searchMode)
-    {
+
+
+  listBooks() {
+    this.searchMode = this._activatedRoute.snapshot.paramMap.has('keyword');
+    if (this.searchMode) {
       this.handleSearchBooks();
 
-    }else
-    {
+    } else {
       this.handleListBooks();
     }
   }
 
-  handleListBooks()
-  {
-    const hasCategoryId : boolean = this._activatedRoute.snapshot.paramMap.has('id');
+  handleListBooks() {
+    const hasCategoryId: boolean = this._activatedRoute.snapshot.paramMap.has('id');
 
-    if(hasCategoryId)
+    if (hasCategoryId) {
+      this.currentCategoryId = +this._activatedRoute.snapshot.paramMap.get('id');
+    }
+    else {
+      this.currentCategoryId = 1;
+    }
+//setting up current paget to 1, if user navigates the other categoty
+    if(this.previousCategory != this.currentCategoryId)
     {
-        this.currentCategoryId = +this._activatedRoute.snapshot.paramMap.get('id');
-    }
-    else{
-        this.currentCategoryId = 1;
+      this.currentPage = 1;
     }
 
-    this._bookService.getBooks(this.currentCategoryId).subscribe(
-     data => this.books = data
+    this.previousCategory = this.currentCategoryId;
+
+    this._bookService.getBooks(this.currentCategoryId,this.currentPage -1,this.pageSize).subscribe(
+       this.processPaginate()
       /* data =>{
         console.log(data);
       }*/
     )
   }
 
-  
-  handleSearchBooks()
-  {
-    const keyword : string = this._activatedRoute.snapshot.paramMap.get('keyword');
-    this._bookService.searchBooks(keyword).subscribe(
-      data => this.books = data
-       /* data =>{
-         console.log(data);
-       }*/
-     )
+
+  handleSearchBooks() {
+    const keyword: string = this._activatedRoute.snapshot.paramMap.get('keyword');
+    this._bookService.searchBooks(keyword, this.currentPage -1,this.pageSize).subscribe(
+      this.processPaginate()
+      //data => this.books = data
+      /* data =>{
+        console.log(data);
+      }*/
+    )
   }
 
+  updatePageSize(pageSize: number) {
+    this.pageSize = pageSize;
+    this.currentPage = 1;
+    this.listBooks();
+  }
 
-/*
-  books: Book[] =
-  [
+  processPaginate()
+  {
+    return data =>
     {
-      "sku": "text-100",
-      "name": "C Programming Language",
-      "description": "Learn C Programming Language",
-      "unitPrice": 600.00,
-      "imageUrl": "assets/images/books/text-100.png",
-      "active": true,
-      "unitsInStock": 100,
-      "createdOn": new Date(),
-      "updatedOn": null
-  },
-  {
-      "sku": "text-101",
-      "name": "C# Crash Course",
-      "description": "Learn C# Programming Language",
-      "unitPrice": 900.00,
-      "imageUrl": "assets/images/books/text-101.png",
-      "active": true,
-      "unitsInStock": 100,
-      "createdOn": new Date(),
-      "updatedOn": null
-  },
-  {
-      "sku": "text-102",
-      "name": "C++ Crash Course",
-      "description": "Learn C++ Programming Language",
-      "unitPrice": 700.00,
-      "imageUrl": "assets/images/books/text-102.png",
-      "active": true,
-      "unitsInStock": 100,
-      "createdOn": new Date(),
-      "updatedOn": null
-  },
-  {
-      "sku": "text-103",
-      "name": "Cracking The Coding Interview",
-      "description": "Learn Cracking the coding interview",
-      "unitPrice": 1000.00,
-      "imageUrl": "assets/images/books/text-103.png",
-      "active": true,
-      "unitsInStock": 100,
-      "createdOn": new Date(),
-      "updatedOn": null
-  },
-  {
-      "sku": "text-104",
-      "name": "Data Structures And Algorithms",
-      "description": "Learn data structures and algorithms",
-      "unitPrice": 1200.00,
-      "imageUrl": "assets/images/books/text-104.png",
-      "active": true,
-      "unitsInStock": 100,
-      "createdOn": new Date(),
-      "updatedOn": null
-  },
-  {
-      "sku": "text-105",
-      "name": "Head First Design Patterns",
-      "description": "Learn design patterns from head first",
-      "unitPrice": 600.00,
-      "imageUrl": "assets/images/books/text-105.png",
-      "active": true,
-      "unitsInStock": 100,
-      "createdOn": new Date(),
-      "updatedOn": null
-  },
-  {
-      "sku": "text-106",
-      "name": "Java Programming",
-      "description": "Learn java programming from scratch",
-      "unitPrice": 800.00,
-      "imageUrl": "assets/images/books/text-106.png",
-      "active": true,
-      "unitsInStock": 100,
-      "createdOn": new Date(),
-      "updatedOn": null
-  },
-  {
-      "sku": "text-107",
-      "name": "ML With Python",
-      "description": "Learn machine learning with python",
-      "unitPrice": 1500.00,
-      "imageUrl": "assets/images/books/text-107.png",
-      "active": true,
-      "unitsInStock": 100,
-      "createdOn": new Date(),
-      "updatedOn": null
-  },
-  {
-      "sku": "text-108",
-      "name": "Practical API Design",
-      "description": "Learn how to design API practically",
-      "unitPrice": 999.00,
-      "imageUrl": "assets/images/books/text-108.png",
-      "active": true,
-      "unitsInStock": 100,
-      "createdOn": new Date(),
-      "updatedOn": null
-  },
-  {
-      "sku": "text-109",
-      "name": "Python Programming",
-      "description": "Learn python programming from scratch",
-      "unitPrice": 499.00,
-      "imageUrl": "assets/images/books/text-109.png",
-      "active": true,
-      "unitsInStock": 100,
-      "createdOn": new Date(),
-      "updatedOn": null
+      this.books = data._embedded.books;
+      // page from start from 1 index 
+      this.currentPage = data.page.number + 1;
+      this.totalRecords =   data.page.totalElements;
+      this.pageSize = data.page.size;
+    } 
   }
-
-  ]
-
-*/
+  /*
+    books: Book[] =
+    [
+      {
+        "sku": "text-100",
+        "name": "C Programming Language",
+        "description": "Learn C Programming Language",
+        "unitPrice": 600.00,
+        "imageUrl": "assets/images/books/text-100.png",
+        "active": true,
+        "unitsInStock": 100,
+        "createdOn": new Date(),
+        "updatedOn": null
+    },
+    {
+        "sku": "text-101",
+        "name": "C# Crash Course",
+        "description": "Learn C# Programming Language",
+        "unitPrice": 900.00,
+        "imageUrl": "assets/images/books/text-101.png",
+        "active": true,
+        "unitsInStock": 100,
+        "createdOn": new Date(),
+        "updatedOn": null
+    },
+    {
+        "sku": "text-102",
+        "name": "C++ Crash Course",
+        "description": "Learn C++ Programming Language",
+        "unitPrice": 700.00,
+        "imageUrl": "assets/images/books/text-102.png",
+        "active": true,
+        "unitsInStock": 100,
+        "createdOn": new Date(),
+        "updatedOn": null
+    },
+    {
+        "sku": "text-103",
+        "name": "Cracking The Coding Interview",
+        "description": "Learn Cracking the coding interview",
+        "unitPrice": 1000.00,
+        "imageUrl": "assets/images/books/text-103.png",
+        "active": true,
+        "unitsInStock": 100,
+        "createdOn": new Date(),
+        "updatedOn": null
+    },
+    {
+        "sku": "text-104",
+        "name": "Data Structures And Algorithms",
+        "description": "Learn data structures and algorithms",
+        "unitPrice": 1200.00,
+        "imageUrl": "assets/images/books/text-104.png",
+        "active": true,
+        "unitsInStock": 100,
+        "createdOn": new Date(),
+        "updatedOn": null
+    },
+    {
+        "sku": "text-105",
+        "name": "Head First Design Patterns",
+        "description": "Learn design patterns from head first",
+        "unitPrice": 600.00,
+        "imageUrl": "assets/images/books/text-105.png",
+        "active": true,
+        "unitsInStock": 100,
+        "createdOn": new Date(),
+        "updatedOn": null
+    },
+    {
+        "sku": "text-106",
+        "name": "Java Programming",
+        "description": "Learn java programming from scratch",
+        "unitPrice": 800.00,
+        "imageUrl": "assets/images/books/text-106.png",
+        "active": true,
+        "unitsInStock": 100,
+        "createdOn": new Date(),
+        "updatedOn": null
+    },
+    {
+        "sku": "text-107",
+        "name": "ML With Python",
+        "description": "Learn machine learning with python",
+        "unitPrice": 1500.00,
+        "imageUrl": "assets/images/books/text-107.png",
+        "active": true,
+        "unitsInStock": 100,
+        "createdOn": new Date(),
+        "updatedOn": null
+    },
+    {
+        "sku": "text-108",
+        "name": "Practical API Design",
+        "description": "Learn how to design API practically",
+        "unitPrice": 999.00,
+        "imageUrl": "assets/images/books/text-108.png",
+        "active": true,
+        "unitsInStock": 100,
+        "createdOn": new Date(),
+        "updatedOn": null
+    },
+    {
+        "sku": "text-109",
+        "name": "Python Programming",
+        "description": "Learn python programming from scratch",
+        "unitPrice": 499.00,
+        "imageUrl": "assets/images/books/text-109.png",
+        "active": true,
+        "unitsInStock": 100,
+        "createdOn": new Date(),
+        "updatedOn": null
+    }
   
+    ]
+  
+  */
+
 }
--- angular-bookstore/src/app/services/book.service.ts
+++ angular-bookstore/src/app/services/book.service.ts
@@ -14,36 +14,35 @@ export class BookService {
   private baseUrl = "http://localhost:8080/api/v1/books";
   private bookCategoryUrl = "http://localhost:8080/api/v1/book-category";
 
-  constructor(private httpClient : HttpClient) { }
+  constructor(private httpClient: HttpClient) { }
 
-  getBooks(theCategoryId: number ): Observable<Book[]>{
-    const searchUrl = `${this.baseUrl}/search/categoryid?id=${theCategoryId}`;
-    return this.getBooksList(searchUrl);
+  getBooks(theCategoryId: number,currentPage : number,pageSize : number): Observable<GetResponseBooks> {
+    const searchUrl = `${this.baseUrl}/search/categoryid?id=${theCategoryId}&page=${currentPage}&size=${pageSize}`;
+    return this.httpClient.get<GetResponseBooks>(searchUrl);
 
   }
 
-  getBookCategory( ): Observable<BookCategory[]>{
-    
+  getBookCategory(): Observable<BookCategory[]> {
+
     return this.httpClient.get<GetResponseBookCategory>(this.bookCategoryUrl).pipe(
       map(response => response._embedded.bookCategory)
     );
 
   }
 
-  getBooksList(searchUrl : string)
-  {
+  getBooksList(searchUrl: string) {
     return this.httpClient.get<GetResponseBooks>(searchUrl).pipe(
       map(response => response._embedded.books)
     );
   }
-  searchBooks(keyword: String ): Observable<Book[]>{
-    const searchUrl = `${this.baseUrl}/search/searchbykeyword?name=${keyword}`;
-   
-    return this.getBooksList(searchUrl);
+  searchBooks(keyword: String,currentPage : number , pageSize : number ): Observable<GetResponseBooks> {
+    const searchUrl = `${this.baseUrl}/search/searchbykeyword?name=${keyword}&page=${currentPage}&size=${pageSize}`;
+
+    return this.httpClient.get<GetResponseBooks>(searchUrl);
 
   }
 
-  get(bookId : number) : Observable<Book>{
+  get(bookId: number): Observable<Book> {
 
     const bookDetilsUrl = `${this.baseUrl}/${bookId}`;
     return this.httpClient.get<Book>(bookDetilsUrl);
@@ -51,16 +50,27 @@ export class BookService {
 
 }
 
-interface GetResponseBooks{
-  _embedded : {
-    books : Book[];
+interface GetResponseBooks {
+  _embedded: {
+    books: Book[];
   }
 }
 
 
-interface GetResponseBookCategory{
-  _embedded : {
-    bookCategory : BookCategory[];
+interface GetResponseBookCategory {
+  _embedded: {
+    bookCategory: BookCategory[];
+  }
+  page: {
+    //number of records in each page.
+    size: number,
+    //total number of records in database
+    totalElements: number,
+    //total number of pages , starts from 0 index
+    totalPages: number,
+    //current page
+    number: number
+
   }
 }
 
